d24964f01ae2ec0c1f1e77a61bb51c67ea4f298a
Edoardo Wijaya Grappolini <ewgrappolini@gmail.com>
2022-05-15 21:17:59 +0200

    commit ui plane and basic features detection

---
+++ Guide_FI/Assets/Scripts/ToggleAR.cs
@@ -0,0 +1,31 @@
+
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ToggleAR : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ARPlaneManager planeManager;
+    public ARPointCloudManager pointCloudManager;
+
+    public void OnValueChanged(bool isOn) {
+        VisualizePlanes(isOn);
+        VisualizePoints(isOn);
+    }
+
+    void VisualizePlanes(bool active) {
+        planeManager.enabled = active;
+        foreach (ARPlane plane in planeManager.trackables) {
+            plane.gameObject.SetActive(active);
+        }
+    }
+
+    void VisualizePoints(bool active)
+    {
+        pointCloudManager.enabled = active;
+        foreach (ARPointCloud point in pointCloudManager.trackables)
+        {
+            point.gameObject.SetActive(active);
+        }
+    }
+}
